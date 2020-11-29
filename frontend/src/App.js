ffe1bc6f60229105c5001b34359475a74a0b41ce
ddscarneiro <ddscarneiro@outlook.com>
2020-11-29 17:03:56 -0300

    Added user routes and home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Routes from './Routes'
+
 function App() {
   return (
-    <div>
-      nice maluco
-    </div>
+    <Routes />
   );
 }
 
